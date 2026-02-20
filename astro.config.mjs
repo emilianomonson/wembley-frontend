7b29ccec3d32d227f7718d99749fc26a288fcd61
Emiliano Monson <emimonson8gmail.com>
2026-02-19 21:41:44 -0300

    Agregando el adaptador de Vercel para SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // ¡ESTA ES LA LÍNEA MÁGICA QUE ENCIENDE EL SSR!
+  // ¡ESTA ES LA LÍNEA MÁGICA QUE ENCIENDE EL SSR!
+  output: 'server',
+
   integrations: [tailwind()],
+  adapter: vercel(),
 });
